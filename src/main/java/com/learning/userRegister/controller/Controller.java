c66f0a4a11b2544040312a280731eb0d750dde84
jiahuixi <791226357@qq.com>
2018-07-09 14:40:53 +0800

    Initial commit

---
+++ src/main/java/com/learning/userRegister/controller/Controller.java
@@ -0,0 +1,11 @@
+package com.learning.userRegister.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Controller {
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World";
+	}
+}
