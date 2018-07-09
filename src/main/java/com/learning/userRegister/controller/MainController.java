71ba459731bf4325e719d6db23e21c1da487d9b1
jiahuixi <791226357@qq.com>
2018-07-09 15:54:55 +0800

    sssssssss

---
+++ src/main/java/com/learning/userRegister/controller/MainController.java
@@ -0,0 +1,19 @@
+package com.learning.userRegister.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("/index")
+public class MainController {
+	@RequestMapping("/index")
+	public String index(HashMap<String, Object> map) {
+        //获取配置文件中的信息vv
+        //传到template模板中（会把模板中能找到的所有的ftl文件中的名称都替换掉）
+        map.put("hello", "欢迎进入HTML页面");
+        //返回页面hello.ftl
+        return "index";
+	}
+}
