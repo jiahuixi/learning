569569a3ed6eb37891b93b1eb919b5fc7ef61cb9
jiahuixi <791226357@qq.com>
2018-08-30 10:10:54 +0800

    sssssssss

--- src/main/java/com/learning/userRegister/DemoApplication.java
+++ src/main/java/com/learning/userRegister/DemoApplication.java
@@ -3,15 +3,25 @@ package com.learning.userRegister;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @SpringBootApplication
 @EnableTransactionManagement//事务注解
 @MapperScan("com.learning.userRegister.dao")
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+	//public static void main(String[] args) {
+	//	SpringApplication.run(DemoApplication.class, args);
+	//}
 }
